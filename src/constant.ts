e15e60d24f578f115928fe99483fd5df09cc268e
HHK <hhk.contact@protonmail.com>
2022-01-29 19:00:59 +0100

    feat: update nft img

--- src/constant.ts
+++ src/constant.ts
@@ -15,7 +15,7 @@ const AIRDROPS: IAirdrop[] = [
     id: 0,
     title: 'Chinese new year',
     expiry: 1644793200,
-    img: 'https://lh3.googleusercontent.com/SVHvcHJNLweNf5Pv0OxAi9aqfDZSMkZBHvPS7OOWHvCy4X_pnNogzivftvI97_12qKsDZ7D_RAYPNs4k2zbD5DkfnQGN0BeoN-sE=w600',
+    img: 'https://lh3.googleusercontent.com/uCWSiOdAs_dhwS9drY1dtjwBy-ihJG5ay0l31QDmyT4FwFBmsJl1lj32jjM9iCJEB7f5S33-OORePz2FLZXDQ7cgvsxBohQCPYFrZ_Y=w600',
     merkle: merkle0,
   },
 ];
